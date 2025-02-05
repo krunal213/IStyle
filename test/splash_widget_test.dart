e1c3b55f6df25515f14edbe713e29c0bd3a72039
krunal213 <43728631+krunal213@users.noreply.github.com>
2025-02-05 20:38:16 +0530

    Update splash_widget_test.dart

--- test/splash_widget_test.dart
+++ test/splash_widget_test.dart
@@ -18,7 +18,7 @@ void main() {
     ));
 
     Finder image_widget_background_finder = find.byKey(Key("image_widget_background"));
-    expect(null, findsOneWidget);
+    expect(image_widget_background_finder, findsOneWidget);
     final image_widget_background = tester.widget<Image>(image_widget_background_finder);
     expect(image_widget_background.image, isA<AssetImage>());
     expect((image_widget_background.image as AssetImage).assetName, 'assets/images/ic_welcome_background.png');
